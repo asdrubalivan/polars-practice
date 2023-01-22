6dad2ca8cfaedf6e8175712f128e0e6075cc2d5a
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2023-01-21 21:59:51 -0400

    Finishing query

--- main.py
+++ main.py
@@ -10,8 +10,20 @@ Results would be like, countries where Argentina visa is required but not Venezu
 Country | Passport
 """
 
-ven_arg_query = df_csv.filter(
+needs_visa = df_csv.filter(
     (pl.col("Passport") == "VEN") | (pl.col("Passport") == "ARG")
-).with_column(pl.col("Requirement").str.contains(r"visa|covid").alias("needs_visa"))
+).with_column(
+    pl.col("Requirement")
+    .str.contains(r"required|covid|e\-visa|arrival")
+    .alias("needs_visa")
+)
 
-print(ven_arg_query.collect())
+venezuelan_visas = needs_visa.filter(pl.col("Passport") == "VEN")
+arg_visas = needs_visa.filter(pl.col("Passport") == "ARG")
+
+total = venezuelan_visas.join(arg_visas, on="Destination")
+f = total.filter((pl.col("needs_visa") == True) & (pl.col("needs_visa_right") == False))
+
+pl.Config.set_tbl_rows(100)
+
+print(f.collect())
