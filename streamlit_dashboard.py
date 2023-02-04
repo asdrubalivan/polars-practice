efd089a35e41a18a8b167fe4f7942a5e8a6d202c
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2023-02-04 11:19:12 -0400

    Adding swap

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -3,11 +3,19 @@ from backend import get_visa_countries, get_country_list
 
 countries = get_country_list()
 
-choice1 = st.selectbox(label="Country 1", options=countries.to_pandas(), key="country1")
-choice2 = st.selectbox(label="Country 2", options=countries.to_pandas(), key="country2")
+
+def swap() -> None:
+    st.session_state.country1, st.session_state.country2 = (
+        st.session_state.country2,
+        st.session_state.country1,
+    )
 
 
 st.title("Visa requirements in different countries")
+choice1 = st.selectbox(label="Country 1", options=countries.to_pandas(), key="country1")
+choice2 = st.selectbox(label="Country 2", options=countries.to_pandas(), key="country2")
+st.button(label="Swap", on_click=swap)
+
 
 if st.session_state.country1 == st.session_state.country2:
     st.text("Countries are the same, please select different countries")
