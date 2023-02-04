8313a943d1ebdd7d411c97f7468a1594acc0135f
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2023-02-04 09:51:07 -0400

    Refactored function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import polars as pl
 
 file = "./passport-index-dataset/passport-index-tidy-iso3.csv"
 
-df_csv = pl.scan_csv(file)
+pl.Config.set_tbl_rows(100)
 
 """
 Results would be like, countries where Argentina visa is required but not Venezuelan
@@ -10,20 +10,26 @@ Results would be like, countries where Argentina visa is required but not Venezu
 Country | Passport
 """
 
-needs_visa = df_csv.filter(
-    (pl.col("Passport") == "VEN") | (pl.col("Passport") == "ARG")
-).with_column(
-    pl.col("Requirement")
-    .str.contains(r"required|covid|e\-visa|arrival")
-    .alias("needs_visa")
-)
 
-venezuelan_visas = needs_visa.filter(pl.col("Passport") == "VEN")
-arg_visas = needs_visa.filter(pl.col("Passport") == "ARG")
+def get_visa_countries(country1: str, country2: str):
+    df_csv = pl.scan_csv(file)
+    needs_visa = df_csv.filter(
+        (pl.col("Passport") == country1) | (pl.col("Passport") == country2)
+    ).with_column(
+        pl.col("Requirement")
+        .str.contains(r"required|covid|e\-visa|arrival")
+        .alias("needs_visa")
+    )
 
-total = venezuelan_visas.join(arg_visas, on="Destination")
-f = total.filter((pl.col("needs_visa") == True) & (pl.col("needs_visa_right") == False))
+    country_1_visas = needs_visa.filter(pl.col("Passport") == country1)
+    country_2_visas = needs_visa.filter(pl.col("Passport") == country2)
 
-pl.Config.set_tbl_rows(100)
+    total = country_1_visas.join(country_2_visas, on="Destination")
+    f = total.filter(
+        (pl.col("needs_visa") == True) & (pl.col("needs_visa_right") == False)
+    )
 
-print(f.collect())
+    return f.collect()
+
+
+print(get_visa_countries("VEN", "ARG"))
